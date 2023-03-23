ae49a816e30dc4caa149dd5eecb78234e48bbb58
cuber-dev <118879829+cuber-dev@users.noreply.github.com>
2023-03-23 16:02:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const rangeOutput = document.querySelector("#range-output");
+const range = document.querySelector("#char-limit");
+const generateBtn = document.querySelector("#generate-pass");
+const passDisplay = document.querySelector("#pass-text");
+const copyBtn = document.querySelector("#copy-btn");
+
+let charLimit = 8;
+let characters = "ABCDEFGHIJKLMOPQRSTUVWXYZabscdefghijklmnopqrstuvwxyz123456789!@#$%(_)/\|:^&*-?";
+
+range.addEventListener("change", changeCharLimitDisplay);
+generateBtn.addEventListener("click",generatePassword);
+copyBtn.addEventListener("click",copyPassword);
+
+function changeCharLimitDisplay(){
+    let limit = range.value;
+    rangeOutput.innerText = "Characters : " + limit;
+    charLimit = limit;
+}
+
+function generatePassword(){
+    let password = "";
+    for(let i=0; i < charLimit ; i++){
+        password += characters[Math.floor(Math.random() * characters.length)];
+    }
+    passDisplay.innerText = password;
+}
+
+function copyPassword(){
+    const textArea = document.createElement("textarea");
+    textArea.value = passDisplay.innerText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+
+    const copyDisplayMark = document.createElement("span");
+    copyDisplayMark.innerHTML = "Copied to Clipboard!";
+    copyDisplayMark.classList.add("copyDisplayMark");
+
+    document.body.appendChild(copyDisplayMark);
+
+    setTimeout(() => {
+        copyDisplayMark.remove();
+    },3000);
+}
+
+
+window.addEventListener("load", () => {
+    generatePassword();
+})
